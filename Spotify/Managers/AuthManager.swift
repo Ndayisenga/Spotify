43c03f9b627633010cf4d85aa47f81930e0b2df6
simplilearn_github <cloude056@yahoo.fr>
2021-02-23 18:16:04 +0200

    Spotify M2

--- Spotify/Managers/AuthManager.swift
+++ Spotify/Managers/AuthManager.swift
@@ -22,7 +22,7 @@ final class AuthManager {
         let scopes = "user-read-private"
         let redirectURI = "https://blesseddairies.wixsite.com/rvit"
         let base = "https://accounts.spotify.com/authorize"
-        let string = "\(base)?response_type=code&client_id=\(Constants.clientID)&scope=\(scopes)&redirect_uri=\(redirectURI)"
+        let string = "\(base)?response_type=code&client_id=\(Constants.clientID)&scope=\(scopes)&redirect_uri=\(redirectURI)&show_dialog=TRUE"
         return URL(string: string)
     }
     
@@ -42,6 +42,16 @@ final class AuthManager {
     private var shouldRefreshToken: Bool {
         
         return false
+        
+    }
+    public func exchangeCodeForToken(code: String, completion: @escaping ((Bool) -> Void) ) {
+        
+        // get token
+    }
+    public func refreshAccessToken() {
+        
+    }
+    private func cacheToken() {
         
     }
 }
