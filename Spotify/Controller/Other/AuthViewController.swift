58c44efa6c3023f496223ab9ef56cc12af6cf4db
simplilearn_github <cloude056@yahoo.fr>
2021-02-25 19:05:18 +0200

    Spotify MO

--- Spotify/Controller/Other/AuthViewController.swift
+++ Spotify/Controller/Other/AuthViewController.swift
@@ -9,7 +9,7 @@ import UIKit
 import WebKit
 
 class AuthViewController: UIViewController, WKNavigationDelegate {
-
+    
     private let webView: WKWebView = {
         let prefs = WKWebpagePreferences()
         prefs.allowsContentJavaScript = true
@@ -30,7 +30,7 @@ class AuthViewController: UIViewController, WKNavigationDelegate {
         guard  let url = AuthManager.shared.signInURL else {
             return
         }
-
+        
         webView.load(URLRequest(url: url))
     }
     override func viewDidLayoutSubviews() {
@@ -46,6 +46,14 @@ class AuthViewController: UIViewController, WKNavigationDelegate {
         guard let code = URLComponents(string: url.absoluteString)?.queryItems?.first(where: {$0.name == "code" })?.value else {
             return
         }
+        webView.isHidden = true
+        
         print("Code: \(code)")
+        AuthManager.shared.exchangeCodeForToken(code: code) { [weak self] success in
+            DispatchQueue.main.async {
+                self?.navigationController?.popToRootViewController(animated: true)
+                self?.completionHandler?(success)
+            }
+        }
     }
 }  
---
+++ Spotify/Models/AuthResponse.swift
@@ -0,0 +1,18 @@
+//
+//  AuthResponse.swift
+//  Spotify
+//
+//  Created by Jean Claude Ndayisenga on 25/02/2021.
+//
+
+import Foundation
+
+ struct  AuthResponse: Codable {
+    let access_token: String
+    let expires_in: Int
+    let refresh_token: String
+    let scope: String
+    let token_type: String
+}
+
+
